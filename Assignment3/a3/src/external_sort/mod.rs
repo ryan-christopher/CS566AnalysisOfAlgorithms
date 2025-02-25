b1be9b63fc7a4fe54a8a70a7ca42c59d6afe5629
Ryan Christopher <rechristopher99@gmail.com>
2025-02-24 22:31:35 -0500

    hw3 work (w.i.p.)

--- Assignment3/a3/src/external_sort/mod.rs
+++ Assignment3/a3/src/external_sort/mod.rs
@@ -1,11 +1,43 @@
 use std::fs;
-use std::io::Read;
+use std::fs::read_to_string;
+use std::io::Write;
+use std::time::Instant;
 
-pub fn test() -> std::io::Result<()> {
-    fs::create_dir("./temp_sort_files")?;
-    Ok(())
-}
+// will need support for: mergesort, heapsort, quicksort, 
+// and binary tree based algorithms
 
-pub fn find_file() {
-//    let mut f = fs::File::open_buffered("./rand_nums.txt")?;
+pub fn sort(method: &str, file: &str) {  
+    let now = Instant::now();
+    
+    //if method == "mergesort" {
+    //    println!("AA");
+    //}
+    // ===== Go through large file, create smaller ones =====
+    /* 
+    let temp_dir = fs::create_dir("./temp_sort_files");
+    let mut counter = 0;
+    let mut temp_file_number = 1;
+    let mut temp_file_name: String = "./temp_sort_files/temp".to_owned();
+    temp_file_name.push_str(&temp_file_number.to_string());
+    let mut temp_file = fs::OpenOptions::new().append(true).create(true).open(&temp_file_name).expect("Can not open file.");
+    for line in read_to_string(file).unwrap().lines() {
+        temp_file.write_all(line.as_bytes()).expect("Can not write to file.");
+        counter += 1;
+        if counter >= 500 {
+            counter = 0;
+            temp_file_number += 1;
+            temp_file_name = "./temp_sort_files/temp".to_owned();
+            temp_file_name.push_str(&temp_file_number.to_string());
+            temp_file = fs::OpenOptions::new().append(true).create(true).open(&temp_file_name).expect("Can not open file.");
+        } 
+    }
+    */
+    // ======================================================
+
+    
+
+    println!("This thing on?");
+    let elapsed = now.elapsed();
+    println!("{:?}", elapsed);
+    
 }
--- Assignment3/a3/src/main.rs
+++ Assignment3/a3/src/main.rs
@@ -1,14 +1,29 @@
 mod stack;
 mod external_sort;
+use std::{fs, io::Write};
+use rand::Rng;
 
 //use rand::Rng;
 
 
 fn main() {
-    stack::stack_tests();
-    //let files = external_sort::test();
-    //println!("{:?}", files);
-//    for x in 1..50 {
-//        print!("{:?} ", rand::rng().random_range(1..100));
-//    }
+    //stack::stack_tests();
+    external_sort::sort("mergesort", "./rand_nums.txt");
+    //let mut file = File::open("./rand_nums.txt");
+    // ===== code used to generate large unsorted file =====
+    /*
+    let mut file = fs::OpenOptions::new().append(true).create(true).open("./rand_nums.txt").expect("Can not open file.");
+    for x in 1..=50000 {
+        let mut curr_line: String = "".to_string();
+        for y in 1..=500 {
+            curr_line += &rand::rng().random_range(1..100000000).to_string();
+            curr_line += " ";
+        }
+        curr_line += "\n";
+        file.write_all(curr_line.as_bytes()).expect("Can not write to file.");
+    }
+    */
+    // =====================================================
+    //let contents = fs::read_to_string("./rand_nums.txt").expect("txt file");
+    //println!("{}", contents);
 }
