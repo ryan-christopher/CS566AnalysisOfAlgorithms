828c0ad66df8f078b0530b93d00a6271e1f3d260
Ryan Christopher <rechristopher99@gmail.com>
2025-02-26 01:36:16 -0500

    Assignment 3 completed

--- Assignment3/a3/src/external_sort/mod.rs
+++ Assignment3/a3/src/external_sort/mod.rs
@@ -1,23 +1,29 @@
 use std::collections::VecDeque;
-use std::fmt::write;
 use std::fs;
 use std::fs::read_to_string;
 use std::io::Write;
 use std::time::Instant;
+use std::collections::BTreeSet;
 
 pub fn sort(method: &str, file: &str) {  
+    // begin timer
     let now = Instant::now();
-    // ===== Go through large file, create smaller ones =====
+
+    // create directory to store intermediate unsorted files
+    // go through large file, create smaller ones with grouping of 5000 lines =====
     fs::create_dir("./temp_unsorted_files").expect("Error creating unsorted folder.");
     let mut counter = 0;
     let mut temp_file_number = 1;
     let mut temp_file_name: String = "./temp_unsorted_files/temp".to_owned();
     temp_file_name.push_str(&temp_file_number.to_string());
     temp_file_name.push_str(".txt");
+
+    // create temp file name dynamically using counter appended to filename
     let mut temp_file = fs::OpenOptions::new().append(true).create(true).open(&temp_file_name).expect("Can not open file.");
     for line in read_to_string(file).unwrap().lines() {
         temp_file.write_all(line.as_bytes()).expect("Can not write to file.");
         counter += 1;
+        // group by 5000 lines
         if counter > 5000 {
             counter = 0;
             temp_file_number += 1;
@@ -27,40 +33,40 @@ pub fn sort(method: &str, file: &str) {
             temp_file = fs::OpenOptions::new().append(true).create(true).open(&temp_file_name).expect("Can not open file.");
         } 
     }
-    // ======================================================
 
-
+    // create directory to store intermediate sorted files
     fs::create_dir("./temp_sorted_files").expect("Error creating sorted folder.");
-    
     let mut temp_sorted_file_number = 1;
+    // for each temp file, create a temp sorted file
     while temp_sorted_file_number <= temp_file_number {
-        // can remove let mut once done, will still be created
         let mut temp_sorted_file_name: String = "./temp_sorted_files/temp".to_owned();
         temp_sorted_file_name.push_str(&temp_sorted_file_number.to_string());
         temp_sorted_file_name.push_str(".txt");
         let mut temp_unsorted_file_name: String = "./temp_unsorted_files/temp".to_owned();
         temp_unsorted_file_name.push_str(&temp_sorted_file_number.to_string());
         temp_unsorted_file_name.push_str(".txt");
-
-
         let mut temp_sorted_file = fs::OpenOptions::new().append(true).create(true).open(&temp_sorted_file_name).expect("Can not open file.");
+
+        // read values from corresponding unsorted file to be written to the sorted file
         let contents = fs::read_to_string(&temp_unsorted_file_name).expect("txt file");
-        // mergesort 
+
+        // mergesort into temp sorted file
         if method == "mergesort" {
             let values: Vec<i32> = contents.trim().split_whitespace().map(|i| i.parse::<i32>().unwrap()).collect();
             let sort_values: String = merge_sort(&values).into_iter().map(|i| i.to_string() + " ").collect();
-            
             temp_sorted_file.write_all(sort_values.as_bytes()).expect("Mergesort error.");
         }
-        // heapsort
+
+        // heapsort into temp sorted file
         else if method == "heapsort" {
             let mut values: Vec<i32> = contents.trim().split_whitespace().map(|i| i.parse::<i32>().unwrap()).collect();
             heapsort(&mut values);
+            let sort_values: String = values.into_iter().map(|i| i.to_string() + " ").collect();
+            temp_sorted_file.write_all(sort_values.as_bytes()).expect("Heapsort error.");
             
         }
 
-
-        // quicksort
+        // quicksort into temp sorted file
         else if method == "quicksort" {
             let mut values: Vec<i32> = contents.trim().split_whitespace().map(|i| i.parse::<i32>().unwrap()).collect();
             let vals_len = values.len() - 1;
@@ -69,15 +75,23 @@ pub fn sort(method: &str, file: &str) {
             temp_sorted_file.write_all(sort_values.as_bytes()).expect("Quicksort error.");
         }
 
-        // binary tree sort
-        else if method == "btree" {
+        // btree sort into temp sorted file
+        if method == "btree" {
+            let values: Vec<i32> = contents.trim().split_whitespace().map(|i| i.parse::<i32>().unwrap()).collect();
+            let mut btree: BTreeSet<i32> = BTreeSet::new();
+            for val in values{
+                btree.insert(val);
+            }
+            let sorted_values: String = btree.iter().map(|i| i.to_string() + " ").collect();
+            temp_sorted_file.write_all(sorted_values.as_bytes()).expect("Btree error");
             
         }
-        temp_sorted_file_number += 1;
-}
-        
 
+        // iterate over counter until all intermediate unsorted files have a sorted file
+        temp_sorted_file_number += 1;
+    }
         
+    // take each sorted file and store as vecdeque to pop first elements
     let mut sorted_file_list: Vec<VecDeque<i32>> = Vec::new();
     for i in 1..=temp_file_number{
         let mut temp_sorted_file_name: String = "./temp_sorted_files/temp".to_owned();
@@ -87,11 +101,15 @@ pub fn sort(method: &str, file: &str) {
         let values: VecDeque<i32> = contents.trim().split_whitespace().map(|i| i.parse::<i32>().unwrap()).collect();
         sorted_file_list.push(values);
     }
-
+    
+    // create sorted_vals file to write results to
     let mut sorted_vals = fs::OpenOptions::new().append(true).create(true).open(&"sorted_vals.txt").expect("Can not open file.");
+    
+    // create array of arbitrary length, store first value from each file into the corresponding index
     let mut merging_array: [Option<i32>; 100] = [Some(0);100];
     let sorted = false;
-
+    
+    // initialize array to Some(0) if a corresponding file exists, otherwise set to None to ignore
     for i in 0..merging_array.len() {
         if merging_array[i] == Some(0) && i < sorted_file_list.len() {
             merging_array[i] = sorted_file_list[i].pop_front();
@@ -100,8 +118,12 @@ pub fn sort(method: &str, file: &str) {
             merging_array[i] = None;
         }
     }
+    
+    // create variable to store result line to write to
     let mut write_count = 0;
     let mut values_line = "".to_owned();
+    
+    // continue until no elements left in vecdeques
     while sorted == false {
         let mut min_index: isize = -1;
         for element in 0..merging_array.len() {
@@ -113,10 +135,14 @@ pub fn sort(method: &str, file: &str) {
                 min_index = element as isize;
             }
         }
+
+        // no elements exists, elements sorted 
         if min_index == -1 {
             sorted_vals.write_all(values_line.as_bytes()).expect("Unable to write to sorted file.");
             break;
         }
+        
+        // every 1000 elements, write to file and start new line
         if write_count < 1000 {
             write_count += 1;
             values_line += &merging_array[min_index as usize].unwrap().to_string();
@@ -128,17 +154,17 @@ pub fn sort(method: &str, file: &str) {
             sorted_vals.write_all(values_line.as_bytes()).expect("Unable to write to sorted file.");
             values_line = "\n".to_owned();
         }
+        
+        // replace element sorted to file with next element from the corresponding sorted temp file
         merging_array[min_index as usize] = sorted_file_list[min_index as usize].pop_front();
     }
-
-
-
+ 
+    // show elapsed time
     let elapsed = now.elapsed();
     println!("{:?}", elapsed);
-    
 }
 
-// ===== Merge Sort =====
+// ===== mergesort =====
 fn merge_sort(nums: &Vec<i32>) -> Vec<i32> {
     if nums.len() < 2 {
         nums.to_vec()
@@ -183,7 +209,7 @@ fn merge(left_half: &Vec<i32>, right_half: &Vec<i32>) -> Vec<i32> {
     merged_vals
 }
 
-// ===== Heap Sort =====
+// ===== heapsort =====
 fn heapsort<T>(arr: &mut [T])
 where
     T: Copy + Clone + PartialEq + PartialOrd,
@@ -222,7 +248,7 @@ where
     }
 }
 
-// ===== Quick Sort =====
+// ===== quicksort =====
 fn quick_sort(nums: &mut [i32], start: usize, end: usize) {
     if start < end {
         let pivot = sort_pivot(nums, start, end);
@@ -247,5 +273,3 @@ fn sort_pivot(nums: &mut [i32], start: usize, end: usize) -> usize {
     nums.swap((index + 1) as usize, pivot);
     (index + 1) as usize
 }
-
-// ===== Binary Tree Sort =====
--- Assignment3/a3/src/main.rs
+++ Assignment3/a3/src/main.rs
@@ -2,13 +2,11 @@ mod stack;
 mod external_sort;
 use std::{fs, io::Write};
 use rand::Rng;
-
 //use rand::Rng;
 
 
 fn main() {
-    //let mut file = File::open("./rand_nums.txt");
-    // ===== code used to generate large unsorted file =====
+    // ===== uncomment this code to generate large unsorted file =====
     /*
     let mut file = fs::OpenOptions::new().append(true).create(true).open("./rand_nums.txt").expect("Can not open file.");
     for x in 1..=50000 {
@@ -21,6 +19,14 @@ fn main() {
         file.write_all(curr_line.as_bytes()).expect("Can not write to file.");
     }
     */
-    //stack::stack_tests();
+    
+    stack::stack_tests();
+    println!("===== Mergesort =====");
+    external_sort::sort("mergesort", "./rand_nums.txt");
+    println!("===== Heapsort =====");
+    external_sort::sort("heapsort", "./rand_nums.txt");
+    println!("===== Quicksort =====");
     external_sort::sort("quicksort", "./rand_nums.txt");
+    println!("===== B-tree Sort =====");
+    external_sort::sort("btree", "./rand_nums.txt");
 }
